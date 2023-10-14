2ffe9ba0dc6b4e7d662be169c61313448437055c
hayami <goohayami@gmail.com>
2023-10-14 23:51:53 +0900

    asdfa

--- app/api/hello/route.js
+++ app/api/hello/route.js
@@ -3,7 +3,7 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
   return NextResponse.json({
     name: "hayami",
-    age: 52,
+    age: "52",
     country: "Japan",
   });
 }
