fd03e948de92452ebb18755b028acefb99d8cb1a
hayami <goohayami@gmail.com>
2023-10-15 13:33:15 +0900

    one

--- app/api/todos/route.js
+++ app/api/todos/route.js
@@ -1,21 +1,21 @@
-import { NextResponse } from "next/server";
+// import { NextResponse } from "next/server";
 
-export async function GET() {
-  return NextResponse.json([
-    {
-      name: "焼肉",
-      description: "ハラミが好きです",
-      price: 3800,
-    },
-    {
-      name: "ステーキ",
-      description: "ミディアムが普通の焼き方です",
-      price: 5000,
-    },
-    {
-      name: "インドカレー",
-      description: "サグ系にハマっています",
-      price: 1400,
-    },
-  ]);
-}
+// export async function GET() {
+//   return NextResponse.json([
+//     {
+//       name: "焼肉",
+//       description: "ハラミが好きです",
+//       price: 3800,
+//     },
+//     {
+//       name: "ステーキ",
+//       description: "ミディアムが普通の焼き方です",
+//       price: 5000,
+//     },
+//     {
+//       name: "インドカレー",
+//       description: "サグ系にハマっています",
+//       price: 1400,
+//     },
+//   ]);
+// }
--- app/page.js
+++ app/page.js
@@ -1,11 +1,11 @@
 import About from "./About/page";
-import Challenge from "./Challenge/page";
+// import Challenge from "./Challenge/page";
 
 export default function Home() {
   return (
     <>
       <About />
-      <Challenge />
+      {/* <Challenge /> */}
     </>
   );
 }
