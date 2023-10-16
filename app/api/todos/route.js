9fd5344dfbee4ea5944b87359cbe6840f8d6ab38
hayami <goohayami@gmail.com>
2023-10-16 19:14:56 +0900

    add-visible

--- app/api/todos/route.js
+++ app/api/todos/route.js
@@ -6,21 +6,25 @@ export async function GET(request) {
       name: "焼肉",
       description: "ハラミが好きです",
       price: 3800,
+      website: "https://goohayami.com/",
     },
     {
       name: "ステーキ",
       description: "ミディアムが普通の焼き方です",
       price: 5000,
+      website: "https://www.yahoo.co.jp/",
     },
     {
       name: "インドカレー",
       description: "サグ系にハマっています",
       price: 1400,
+      website: "https://www.google.co.jp/",
     },
     {
       name: "親子丼",
       description: "新鮮卵を使っています",
       price: 1000,
+      website: "https://www.netflix.com/browse",
     },
   ]);
 }
