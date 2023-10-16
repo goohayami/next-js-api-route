a458349e92b26c8c8ffd81295c6c684d22fd2ce5
hayami <goohayami@gmail.com>
2023-10-16 20:07:40 +0900

    website

--- app/api/todos/route.js
+++ app/api/todos/route.js
@@ -3,26 +3,26 @@ import { NextResponse } from "next/server";
 export async function GET(request) {
   return NextResponse.json([
     {
-      name: "焼肉",
-      description: "ハラミが好きです",
-      price: 3800,
+      name: "Goohayami",
+      description: "JavaScriptメインの初心者ブログ",
+      price: "★★★",
       website: "https://goohayami.com/",
     },
     {
-      name: "ステーキ",
-      description: "ミディアムが普通の焼き方です",
-      price: 5000,
+      name: "Yahoo Japan",
+      description: "老舗の検索サイト",
+      price: "★★★★",
       website: "https://www.yahoo.co.jp/",
     },
     {
-      name: "インドカレー",
-      description: "サグ系にハマっています",
-      price: 1400,
+      name: "Google",
+      description: "みんな大好きグーグル先生",
+      price: "★★★★★",
       website: "https://www.google.co.jp/",
     },
     {
-      name: "親子丼",
-      description: "新鮮卵を使っています",
+      name: "NetFlix",
+      description: "コロナのお供に",
       price: 1000,
       website: "https://www.netflix.com/browse",
     },
